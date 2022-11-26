4c7a37dde385f912aae91ea490b67097d3fc8d07
QemerS <tu201806169@code.edu.az>
2022-11-26 18:34:11 +0400

    addding count of products to the badge of cart button

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
 const cartBtn = document.getElementById('cart-btn');
+const numberOFItemsInCart = cartBtn.children[1];
 const addToCartBtns = [...document.getElementsByClassName('add-to-cart-btn')];
 let idArr =  [...localStorage.getItem('cart')];
 
 // when we delete items from arr their ',' can left in arr,
 // to include only ids in arr we should do this
-idArr = idArr.filter(e => e != ',');
+idArr = idArr.filter(e => e != ',');git
+addCountToCartBUtton();
 
 addToCartBtns.forEach(btn => {
     // add colors to the buttons by condition
@@ -22,12 +24,19 @@ addToCartBtns.forEach(btn => {
             idArr.splice(idArr.indexOf(idOfBtn), 1);
             //change the button's color
             this.classList.replace('btn-success', 'btn-primary');
+            addCountToCartBUtton();
         } else {
             // add not existed id
             idArr.push(idOfBtn);
             //change the button's color
             this.classList.replace('btn-primary', 'btn-success');
+            addCountToCartBUtton();
         }
         const cartInfo = localStorage.setItem('cart', idArr);
     })
 });
+
+// adding number to the cart button
+function addCountToCartBUtton(params) {
+    numberOFItemsInCart.textContent = idArr.length;
+}
